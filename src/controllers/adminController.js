eed1781d8335756dbaf953da957871372274889e
muhammadsyifaaul <muhammadsyifaaulj@gmail.com>
2024-08-17 14:02:22 +0700

    update

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -25,8 +25,6 @@ exports.createPage = async (req,res) => {
 
 exports.addData = async (req,res) => {
     const { title,libraryName,authorName } = req.body;
-    // console.log(title,libraryName,author)
-
     const book = new Book({
         title
     })
@@ -37,5 +35,28 @@ exports.addData = async (req,res) => {
 
     const author = new Author({
         name: authorName
+    })
+
+    await Promise.all([book.save(),library.save(),author.save()])
+    .then(res => console.log('data added successfully'))
+    .catch(err => console.log(err))
+
+    author.books.push(book._id)
+    library.books.push(book._id)
+
+    await Promise.all([author.save(),library.save()])
+            .then(res => console.log('added author to book & added books to library'))
+            .catch(err => console.log(err))
+            res.redirect('/admin')
+}
+
+exports.detailsBook = async (req,res) => {
+    const {id} = req.params
+    const book = await Book.findById(id).populate('author', 'name')
+    // console.log(book)
+    res.render('admin/book', {
+        title: 'Details',
+        layout: 'layouts/adminLayout',
+        book
     })
 }
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const { adminDash, createPage, addData } = require('../controllers/adminController');
+const { adminDash, createPage, addData, detailsBook } = require('../controllers/adminController');
 const router = express.Router();
 
 router.get('/admin',adminDash)
 router.get('/createData',createPage)
 router.post('/addData',addData)
-
+router.get('/details/book/:id',detailsBook)
+router.get('/details/library/:id',(req,res) => {
+    res.send('hello')
+})
+router.get('/details/author/:id',(req,res) => {
+    res.send('hello')
+})
 
 module.exports = router
